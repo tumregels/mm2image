6f0a9dac5d3b312b4b53de7cddab6178e934dc99
tumregels <4774418+tumregels@users.noreply.github.com>
2023-09-15 15:24:40 -0400

    Move diagram text into file

---
+++ file1.mm
@@ -0,0 +1,6 @@
+graph LR;
+    A--> B & C & D;
+    B--> A & E;
+    C--> A & E;
+    D--> A & E;
+    E--> B & C & D;
--- mm2image.py
+++ mm2image.py
@@ -1,10 +1,12 @@
 import base64
-from textwrap import dedent
+from pathlib import Path
+from pprint import pprint
 
 from html2image import Html2Image
 
 
 def mm(diagram: str, save_as: str) -> str:
+    pprint(diagram)
     graph_bytes = diagram.encode("ascii")
 
     base64_bytes = base64.b64encode(graph_bytes)
@@ -13,17 +15,15 @@ def mm(diagram: str, save_as: str) -> str:
 
     hti = Html2Image()
     hti.screenshot(url=url, save_as=save_as)
+    print(url)
     return url
 
 
 def main() -> None:
-    mm(dedent("""\
-    graph LR;
-        A--> B & C & D;
-        B--> A & E;
-        C--> A & E;
-        E--> B & C & D;
-    """), 'graph.png')
+    filename = "file1.mm"
+    file = Path(filename)
+    text = file.read_text()
+    mm(text, save_as=file.stem + ".png")
 
 
 if __name__ == '__main__':
